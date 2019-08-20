bc8eca3631ee1f3dd001f26a9074ac3357de9bdc
yotugi-vip <harukipdr@gmail.com>
2019-08-21 07:42:17 +0900

    途中作業コミット

---
+++ musicPlayer.py
@@ -0,0 +1,27 @@
+
+class musicPlayer():
+
+    def __init__( self ):
+
+        playQueue = list()
+        playQueue.append( {"URL":"https://nicovideo", "THUMB":"https://....", "TITLE":"1"} )
+        playQueue.append( {"URL":"https://nicovideo/ssss", "THUMB":"https://....", "TITLE":"2"} )
+        print( playQueue )
+
+    def play( self ):
+        pass
+
+    def skip( self ):
+        pass
+
+    def stop( self ):
+        pass
+
+    def addQueue( self ):
+        pass
+
+    def removeQueue( self ):
+        pass
+    
+    def now( self ):
+        pass
--- nicoRythm.py
+++ nicoRythm.py
@@ -1,2 +1,59 @@
+import discord
 import myToken
 import nico2
+
+client = discord.Client()
+nc = nico2.nico2py()
+vc = None
+
+@client.event
+async def on_ready():
+    print("nicoRythm Logged in")
+    print( client.user.name )
+    print( client.user.id )
+    print("-------------------")
+
+@client.event
+async def on_message(message):
+
+    global vc
+    global nc
+
+    if message.content == "e":
+        await client.logout()
+    
+    elif message.content.startswith("#play"):
+
+        if nc.isDownload == True:
+            await message.channel.send( "now playing! please skip" )
+        
+        else:
+
+            if vc == None:
+                vc = await message.author.voice.channel.connect()
+                await message.channel.send( "connect!" )
+            
+            l = message.content.split(" ")
+        
+            vc.play( discord.FFmpegPCMAudio(  nc.getVideo( l[1] ) ) )
+            vc.source = discord.PCMVolumeTransformer( vc.source )
+            vc.source.volume = 0.05
+
+            await message.channel.send( "play" )
+
+    elif message.content == "#stop":
+
+        vc.stop()
+        await message.channel.send( "stop" )
+
+    elif message.content == "#skip":
+        nc.isDownload = False
+        vc.stop()
+        await message.channel.send( "skip" )
+
+    elif message.content == "dc":
+        await vc.disconnect()
+        vc = None
+        await message.channel.send("disconnect")
+
+client.run( myToken.discord_token )
